1864d231fd91699b31e22b3b34a398afa4a7cee7
kenagy100@gmail.com <kenagy100@gmail.com>
2023-07-15 18:17:29 +0000

    Create Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom';
+import { FaUser } from 'react-icons/fa';
+import './Header.css';
+
+const Header = () => (
+  <header>
+    <div className="navContainer">
+      <Link to="/">
+        <h1>BookStore Cms</h1>
+      </Link>
+      <nav>
+        <Link to="/">Books</Link>
+        <Link to="/categories">Categories</Link>
+      </nav>
+    </div>
+    <div className="user-icon-container">
+      <FaUser />
+    </div>
+  </header>
+);
+
+export default Header;
