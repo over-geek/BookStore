191a3dc23b141b8bbf912db720abda551778f4dc
kenagy100@gmail.com <kenagy100@gmail.com>
2023-07-22 12:13:45 +0000

    Refactor Form.js to use button components

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,9 +1,12 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
+import { PropTypes } from 'prop-types';
 import { addBookItem } from '../../redux/books/booksSlice';
+import Button from '../Button/Button';
+import './Form.css';
 
-function Form() {
+function Form({ btnStyle }) {
   const dispatch = useDispatch();
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
@@ -25,14 +28,25 @@ function Form() {
   };
 
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Book title" />
-        <input type="text" value={author} onChange={(e) => setAuthor(e.target.value)} placeholder="Author" />
-        <button type="submit">Add Book</button>
+    <div className="form-container">
+      <form className="form" onSubmit={handleSubmit}>
+        <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Book title" className="title-input" />
+        <input type="text" value={author} onChange={(e) => setAuthor(e.target.value)} placeholder="Author" className="author-input" />
+        <Button
+          defaultProps
+          btnStyle={btnStyle}
+          name="ADD BOOK"
+        />
       </form>
-    </>
+    </div>
   );
 }
+
+Form.propTypes = {
+  btnStyle: PropTypes.shape({
+    color: PropTypes.string.isRequired,
+    backgroundColor: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default Form;
